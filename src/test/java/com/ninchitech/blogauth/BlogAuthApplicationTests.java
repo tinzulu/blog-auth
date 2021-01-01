539afd5528508b300198f628b67076b6c94335bc
tzulu <tzulu@gmail.com>
2021-01-01 21:42:10 +0200

    initial commit

---
+++ src/test/java/com/ninchitech/blogauth/BlogAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninchitech.blogauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
